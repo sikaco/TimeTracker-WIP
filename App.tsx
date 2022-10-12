250f13c523f0d49dc76e4c6b661f6f23533c66cf
sikaco <sikaco0@gmail.com>
2022-10-13 00:43:07 +0800

    SASS support + debugger added successful.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import styles from "./styles.scss";
+
+const BlueText = () => {
+  return <Text className={styles.blue}>Blue Text</Text>;
+};
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <BlueText />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
