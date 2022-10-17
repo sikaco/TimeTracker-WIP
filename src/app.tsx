92c3a84f1aefc443e0cdc7200cdf07451f568013
sikaco <sikaco0@gmail.com>
2022-10-18 01:14:00 +0800

    updates: Navigation added successful
    + two screens added successful

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,20 @@
-// import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { NavigationContainer } from '@react-navigation/native'
 import * as eva from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
-
-import Homepage from '$pages/homepage'
 import configureStore from '$xuder/store'
+import { AppNavigator } from './navigation'
 import './app.scss'
 
 const store = configureStore()
 
 export default () => (
   <Provider store={store}>
-    <NavigationContainer>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <Homepage></Homepage>
-      </ApplicationProvider>
-    </NavigationContainer>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <AppNavigator />
+    </ApplicationProvider>
   </Provider>
 )
--- src/pages/homepage/component.tsx
+++ src/pages/homepage/component.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
-import { View } from 'react-native'
+import { View, SafeAreaView } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { Layout, Text, Button, Icon } from '@ui-kitten/components'
+import { Layout, Text, Button, Icon, Divider, TopNavigation } from '@ui-kitten/components'
 import { GlobalState } from 'src/xuder/reducer'
 // import cx from 'classnames'
 import { DispatchProps, StateProps } from './types'
@@ -15,9 +15,25 @@ export interface OwnProps extends CommonProps { }
 
 export type Props = StateProps & DispatchProps & Readonly<OwnProps>
 
-const FacebookIcon = (props) => (
-  <Icon name="facebook" {...props} />
-)
+export const HomeScreen = ({ navigation }) => {
+  console.log('123123', navigation)
+
+  const navigateDetails = () => {
+    navigation.navigate('Details')
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <TopNavigation title="MyApp" alignment="center" />
+      <Divider />
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+      </Layout>
+    </SafeAreaView>
+  )
+}
+
+const FacebookIcon = (props) => <Icon name="facebook" {...props} />
 
 export const LoginButton = () => (
   <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
@@ -36,12 +52,14 @@ const Index: React.ForwardRefRenderFunction<{}, Props> = (props, ref) => {
   //   </View>
   // )
 
-  return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <LoginButton />
-      <Text category="h1">HOME</Text>
-    </Layout>
-  )
+  // return (
+  //   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  //     <LoginButton />
+  //     <Text category="h1">HOME</Text>
+  //   </Layout>
+  // )
+
+  // return <HomeScreen {...props} />
 }
 
 export default connect<StateProps, DispatchProps, OwnProps>(
