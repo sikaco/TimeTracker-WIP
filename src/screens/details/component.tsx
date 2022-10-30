2d6952255c63d01d4030ac811862f6cf8442ab4a
sikaco <sikaco0@gmail.com>
2022-10-31 00:47:41 +0800

    feat: homeScreen update + types added.

--- src/screens/details/component.tsx
+++ src/screens/details/component.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
+import { CompositeScreenProps } from '@react-navigation/native'
 import { Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
 
 const BackIcon = (props) => (
   <Icon {...props} name="arrow-back" />
 )
 
-const DetailsScreen = ({ navigation }) => {
+const DetailsScreen = ({ navigation }: CompositeScreenProps<any, any>) => {
   const navigateBack = () => {
     navigation.goBack()
   }
--- src/screens/home/main.tsx
+++ src/screens/home/main.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { View, SafeAreaView } from 'react-native'
+import { CompositeScreenProps } from '@react-navigation/native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Layout, Text, Button, Icon, Divider, TopNavigation } from '@ui-kitten/components'
@@ -11,64 +12,46 @@ import styles from './index.module.scss'
 
 export interface CommonProps { dispatch: any }
 
-export type OwnProps = CommonProps
+export type OwnProps = CommonProps & CompositeScreenProps<any, any>
 
 export type Props = StateProps & DispatchProps & Readonly<OwnProps>
 
-const HomeScreen = ({ navigation }) => {
+// const FacebookIcon = (props) => <Icon name="facebook" {...props} />
+
+// export const LoginButton = () => (
+//   <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
+// )
+
+const HomeScreen: React.ForwardRefRenderFunction<{}, Props> = (props, ref) => {
+  const { actions, a, navigation } = props
+
+  useEffect(() => {
+    actions.test(113)
+  }, [])
+
   const navigateDetails = () => {
     navigation.navigate('Details')
   }
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView ref={ref} style={{ flex: 1, ...styles.container }}>
       <TopNavigation title="MyApp" alignment="center" />
       <Divider />
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Layout className={styles['screen-content']}>
         <Button onPress={navigateDetails}>OPEN DETAILS</Button>
+        <Text className={styles.index}>------{a}</Text>
       </Layout>
     </SafeAreaView>
   )
 }
 
-export default HomeScreen
-
-const FacebookIcon = (props) => <Icon name="facebook" {...props} />
-
-export const LoginButton = () => (
-  <Button accessoryLeft={FacebookIcon}>Login with Facebook</Button>
-)
-
-const Index: React.ForwardRefRenderFunction<{}, Props> = (props, ref) => {
-  const { actions, a } = props
-
-  useEffect(() => {
-    actions.test(111)
-  }, [])
-
-  // return (
-  //   <View ref={ref} style={styles.container}>
-  //     <Text className={styles.index}>------{a}</Text>
-  //   </View>
-  // )
-
-  // return (
-  //   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  //     <LoginButton />
-  //     <Text category="h1">HOME</Text>
-  //   </Layout>
-  // )
-
-  // return <HomeScreen {...props} />
-}
-
-// export default connect<StateProps, DispatchProps, OwnProps>(
-//   (state: GlobalState): StateProps => {
-//     return state.ui.homeScreen
-//   },
-//   (dispatch) => {
-//     return {
-//       actions: bindActionCreators(actions, dispatch),
-//     }
-//   },
-// )(React.forwardRef(Index))
+export default connect<StateProps, DispatchProps, OwnProps>(
+  (state: GlobalState): StateProps => {
+    return state.ui.homeScreen
+  },
+  (dispatch) => {
+    return {
+      actions: bindActionCreators(actions, dispatch),
+    }
+  },
+)(React.forwardRef(HomeScreen))
